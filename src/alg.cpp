014ba4f5a14e5126c9aacba39cd719ec2a67fc67
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-04-03 16:44:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,85 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int priority(char c) {
+  switch (c) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+  }
+  return -1;
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(std::string inf) {
+  TStack <char, 50> stack;
+  std::string result;
+  for (int i = 0; i < inf.size(); i++) {
+    int p = prioryty(inf[i]);
+    if (p == -1) {
+      if (!result.empty() && priority (inf[i - 1]) != -1) {
+        result.push_back(' ');
+      }
+      result.push_back(inf[i]);
+    } else if (p == 0 || p > priority(stack.get()) || stack.isEmpty()) {
+      stack.push(inf[i]);
+    } else {
+      if (p == 1) {
+        while (stack.get() != '(') {
+          result.push_back(' ');
+          result.push_back(stack.get());
+          stack.pop();
+        }
+        stack.pop();
+      } else {
+        while (priority(stack.get()) >= p) {
+          result.push_back(' ');
+          result.push_back(stack.get());
+          stack.pop();
+        }
+        stack.push(inf[i]);
+      }
+    }
+  }
+  while (!stack.isEmpty()) {
+    result.push_back(' ');
+    result.push_back(stack.get());
+    stack.pop();
+  }
+  return result;
+}
+
+int eval(std::string post) {
+  TStack <int, 50> resStack;
+  std::string temp;
+  int a = 0, b = 0;
+  size_t begin = 0, end = 0;
+  for (size_t i = 0; i < post.size(); ++i) {
+    if (post[i] == ' ' || i == post.size()-1) {
+      end = i;
+      if (i ==post.size() - 1)
+        end++;
+      temp = post.substr(begin, end - begin);
+      begin = end + 1;
+      if (isDigit(temp)) {
+        resStack.push(std::stoi(temp));
+      } else {
+        b = resStack.get();
+        resStack.pop();
+        a = resStack.get();
+        resStack.pop();
+        if (temp == '+')
+          resStack.push(a + b);
+        else if (temp == '-')
+          resStack.push(a - b);
+        else if (temp == '*')
+          resStack.push(a * b);
+        else if (temp == '/')
+          resStack.push(a / b);
+      }
+    }
+  }
+  return resStack.get();
 }
